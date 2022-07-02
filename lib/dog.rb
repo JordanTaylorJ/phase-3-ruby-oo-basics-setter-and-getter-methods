6fab5feb169f7ce1dea9ac03839f19991d755b2e
JordanJoseph <jordant.joseph@outlook.com>
2022-07-02 15:21:21 -0600

    commit tree ab8640d3cd20908d8a40a0b8b707e7edcfc846de

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
+    attr_accessor :name
+    attr_reader :breed
 
+    def name=(name)
+        @name = name
+    end 
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    #def initalize (name, breed)
+    #    @name=name
+    #    @breed=breed
+    #end
 end
